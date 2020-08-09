90b0c642fecae515060278cfbca2e1aeddf5662b
Daniel Mor <daniel1mor3055@gmail.com>
2020-08-09 17:37:14 +0300

    update

--- MultiSignal/multi_signal_exceptions.py
+++ MultiSignal/multi_signal_exceptions.py
@@ -1,3 +1,3 @@
 class AmplitudesFrequenciesMatchError(Exception):
-    def __init__(self, message='Amplitudes and frequencies dimensions does not match'):
+    def __init__(self, message='Amplitudes and frequencies dimensions do not match'):
         super().__init__(message)
